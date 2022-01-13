ce0c67056427f1eb32993a74d89f204eaca67903
二两螺蛳粉儿 <zflmiy@qq.com>
2022-01-13 14:36:54 +0800

    update script

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 imageName=`grep -i from Dockerfile|awk '{print $2}'  | awk -F/ '{print $NF}'| awk -F: '{print $1}'`
 
-docker build registry.cn-hangzhou.aliyuncs.com/my_docker_images/images:$imageName .
+docker build -t registry.cn-hangzhou.aliyuncs.com/my_docker_images/images:$imageName .
 docker push registry.cn-hangzhou.aliyuncs.com/my_docker_images/images:$imageName
